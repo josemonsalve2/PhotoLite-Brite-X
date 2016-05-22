781b205e87fc244405a3fa0d5f7a34b2b0143895
bee <bee@ironhide.(none)>
2016-05-22 06:16:58 -0400

    Adding SDK code working with edk final attempt

---
+++ SDK/FinalProjectSDK/Debug/src/xuartlite_selftest_example.d
@@ -0,0 +1,19 @@
+src/xuartlite_selftest_example.d: ../src/xuartlite_selftest_example.c \
+ ../../FinalProjectSDK_bsp/microblaze_0/include/xparameters.h \
+ ../../FinalProjectSDK_bsp/microblaze_0/include/xuartlite.h \
+ ../../FinalProjectSDK_bsp/microblaze_0/include/xil_types.h \
+ ../../FinalProjectSDK_bsp/microblaze_0/include/xil_assert.h \
+ ../../FinalProjectSDK_bsp/microblaze_0/include/xstatus.h \
+ ../../FinalProjectSDK_bsp/microblaze_0/include/xbasic_types.h
+
+../../FinalProjectSDK_bsp/microblaze_0/include/xparameters.h:
+
+../../FinalProjectSDK_bsp/microblaze_0/include/xuartlite.h:
+
+../../FinalProjectSDK_bsp/microblaze_0/include/xil_types.h:
+
+../../FinalProjectSDK_bsp/microblaze_0/include/xil_assert.h:
+
+../../FinalProjectSDK_bsp/microblaze_0/include/xstatus.h:
+
+../../FinalProjectSDK_bsp/microblaze_0/include/xbasic_types.h:
