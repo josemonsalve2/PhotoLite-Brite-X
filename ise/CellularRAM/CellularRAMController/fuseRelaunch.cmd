d83a7b73c1af944898792124f05ee9d3effd2653
Sean Krail <i@seankrail.com>
2016-05-23 13:48:20 -0400

    Deleted a lot of irrelevant files/directories

--- ise/CellularRAM/CellularRAMController/fuseRelaunch.cmd
+++
@@ -1,1 +0,0 @@
--intstyle "ise" -incremental -lib "secureip" -o "/home/bee/git/PhotoLite-Brite-X/ise/CellularRAM/CellularRAMController/CellularRAMController_tb_isim_beh.exe" -prj "/home/bee/git/PhotoLite-Brite-X/ise/CellularRAM/CellularRAMController/CellularRAMController_tb_beh.prj" "work.CellularRAMController_tb" 
--- ise2/ledDriver_LedStrip/fuseRelaunch.cmd
+++
@@ -1,1 +0,0 @@
--intstyle "ise" -incremental -lib "secureip" -o "/home/bee/ledDriver_LedStrip/Led_controller_tb_isim_beh.exe" -prj "/home/bee/ledDriver_LedStrip/Led_controller_tb_beh.prj" "work.Led_controller_tb" 
